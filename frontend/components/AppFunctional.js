39a5ba8b7a85938c23d9570f3cbd923383d9876a
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-07-23 13:29:10 -0700

    Teacher Test Task: Step Counter Complete. Test WIP

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -123,7 +123,8 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{getXYMessage()}</h3>
-        <h3 id="steps">You moved {steps} {steps > 1 ? 'times' : 'time'}</h3>
+        <h3 id="steps">You moved {steps}
+           {steps === 1 ? ' time' : ' times'} </h3>
       </div>
       <div id="grid">
         {
